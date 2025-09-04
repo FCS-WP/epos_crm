a17977b6977a13d3e80800d38dd33f57196a1b5a
shin <nhathanh024@gmail.com>
2025-09-05 00:48:18 +0700

    T15659 Implement this task

--- assets/web/js/Components/LoginForm.js
+++ assets/web/js/Components/LoginForm.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import AuthDialog from "./auth/AuthDialog";
 
-const LoginForm = ({ isOpen,tenant }) => {
+const LoginForm = ({ tenant, isOpen }) => {
   const [open, setOpen] = useState(isOpen);
+
+  const show = () => setOpen(true);
+  const hide = () => setOpen(false);
+
+  if (typeof window !== "undefined") {
+    window.EposLoginForm = { show, hide };
+  }
+
   return (
-    <>
-      <AuthDialog tenant={tenant} open={open} onClose={() => setOpen(false)} />
-    </>
+    <AuthDialog
+      tenant={tenant}
+      open={open}
+      onClose={hide} 
+    />
   );
 };
 
